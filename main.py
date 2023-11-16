070a2f331ffbb8a949effb36b75cf323107d6a83
Emmanuel de La Forest <emmanuel.delaforestdivonne@gmail.com>
2023-11-17 00:26:18 +0900

    updated the main.py after saving the last changes

--- main.py
+++ main.py
@@ -6,14 +6,8 @@ load_dotenv()
 id = os.getenv("newsapi_id")
 url = os.getenv("newsapi_url")
 key = os.getenv("newsapi_key")
-# secured_url = url+key
-# print(secured_url)
-
-# print(id)
-# print(url)
-# print(key)
-
 
 request = requests.get(url+key)
 content = request.text
 print(content)
+S
