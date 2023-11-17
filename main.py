610c43e19a7aa80fc2d3e2c37f01342db0a3d0f0
Emmanuel de La Forest <emmanuel.delaforestdivonne@gmail.com>
2023-11-17 14:11:26 +0900

    accessed the api data and iterate over it

--- main.py
+++ main.py
@@ -2,11 +2,21 @@ import requests
 from dotenv import load_dotenv
 import os
 
+#Load environment variables
 load_dotenv()
+
+#Assigne environment variables
 id = os.getenv("newsapi_id")
 url = os.getenv("newsapi_url")
 key = os.getenv("newsapi_key")
+api_url = url+key
 
-request = requests.get(url+key)
-content = request.text
-print(content)
+#Make a request
+request = requests.get(api_url)
+
+#Get the data within dictionary format
+content = request.json()
+
+#Iterate over articles data
+for article in content["articles"]:
+    print(article["title"])
